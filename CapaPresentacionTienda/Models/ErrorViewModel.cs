aa3e5d7c6898a1de5fe5e9db6f4547742a27e812
rick571z <wilmer_3579@hotmail.com>
2023-04-29 16:30:00 +0200

    Presentación del proyecto

---
+++ CapaPresentacionTienda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CapaPresentacionTienda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
